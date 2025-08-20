cea91ca70ca074b8ae6dd325467bd6f443d1e886
kunteshbastola <163876327+kunteshbastola@users.noreply.github.com>
2025-08-20 16:31:04 +0545

    second

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    director = models.CharField(max_length=50)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=30)
+    description = models.TextField()
+    poster = models.ImageField(upload_to='posters/',blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+    
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Review of {self.movie.title} by {self.user.username}'
--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import  login
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
+from .models import Movie
 # Create your views here.
+def home(request):
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'movies': movies})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('movie_list')
+        else:
+            form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
