6d3ee4d475d8f3056ec5e5b73435fa48f30b4430
kunteshbastola <163876327+kunteshbastola@users.noreply.github.com>
2025-08-20 16:56:42 +0545

    movie

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import  login
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
+from django.contrib.auth import authenticate,login as auth_login
+
 from .models import Movie
 # Create your views here.
 def home(request):
@@ -13,8 +14,23 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request,user)
+            auth_login(request,user)
+            return redirect('movie_list')
+    else:
+        form = UserCreationForm()
+
+
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
             return redirect('movie_list')
         else:
-            form = UserCreationForm()
-        return render(request, 'signup.html', {'form': form})
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+    else:
+        return render(request, 'login.html') 
